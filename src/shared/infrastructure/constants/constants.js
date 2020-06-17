b8c56e43df8c9421663485948cd02788d453851a
Diego Perez <diegopm2000@gmail.com>
2020-06-17 15:12:33 +0200

    fixed constants

--- src/shared/infrastructure/constants/constants.js
+++ src/shared/infrastructure/constants/constants.js
@@ -1,17 +1,5 @@
 // constants.js
 
-// TODO repasar cuales se están usando
-
-exports.TOKEN_NAME = 'tokenwithbearer';
-
 exports.CONFIG_BASEPATH = './config';
 
 exports.APIDOC_BASEPATH = './src/shared/infrastructure/api';
-
-exports.COMMON_HTTP_PROXY_CONTROLLER = 'commonHttpProxyController';
-
-exports.HTTP_COMMON_PRESENTER = 'httpObjectPresenter';
-
-exports.COMMON_PROXY_INFRA = 'commonProxyInfra';
-
-exports.COMMON_PROXY_REPOSITORY = 'commonProxyRepository';
