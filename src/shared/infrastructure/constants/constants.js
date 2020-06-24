7b1d74a449c52f86cbae5d42b8abf3b0b0acced5
Diego Perez <diegopm2000@gmail.com>
2020-06-24 20:06:29 +0200

    deleted not used modules

--- src/shared/infrastructure/constants/constants.js
+++ src/shared/infrastructure/constants/constants.js
@@ -1,11 +1,5 @@
 // constants.js
 
-exports.BOOTSTRAPVARS = [
-  'BOOTSTRAP_CONFIG_SOURCE_APP',
-  'BOOTSTRAP_CONFIG_FILE',
-  'BOOTSTRAP_CONFIG_SPRINGCFG_ENDPOINT',
-];
-
 exports.CONFIG_BASEPATH = './config';
 
 exports.APIDOC_BASEPATH = './src/shared/infrastructure/api';
