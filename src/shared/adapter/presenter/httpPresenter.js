4584c14cf069259739cb17fcb71da3afdbfa3025
Diego Perez <diegopm2000@gmail.com>
2020-06-25 19:13:32 +0200

    improved complexity

--- src/shared/adapter/presenter/httpPresenter.js
+++ src/shared/adapter/presenter/httpPresenter.js
@@ -4,11 +4,19 @@
 // Private Methods
 // //////////////////////////////////////////////////////////////////////////////
 
-const MSG_NOT_AUTHORIZED = { status: 401, data: { code: 401, message: 'User not authorized to resource' } };
-const MSG_NOT_AUTHENTICATED = { status: 403, data: { code: 403, message: 'User not authenticated in the system' } };
-const MGS_OBJECT_NOT_FOUND = { status: 404, data: { code: 404, message: 'Object not found in the system' } };
-const MSG_RELATIONSHIP_NOT_FOUND = { status: 404, data: { code: 404, message: 'Relationship not found in the system' } };
-const MSG_OBJECT_DELETED = { status: 204, data: null };
+const DATA_NOT_AUTHORIZED = { code: 401, message: 'User not authorized to resource' };
+const DATA_NOT_AUTHENTICATED = { code: 403, message: 'User not authenticated in the system' };
+const DATA_OBJECT_NOT_FOUND = { code: 404, message: 'Object not found in the system' };
+const DATA_RELATIONSHIP_NOT_FOUND = { code: 404, message: 'Relationship not found in the system' };
+
+const getFirstErrorMessage = (errorObj) => {
+  if (Array.isArray(errorObj) && errorObj.length > 0) {
+    return errorObj[0];
+  }
+  return errorObj;
+};
+
+const buildDataWithMessage = (code, message) => ({ code, message });
 
 // //////////////////////////////////////////////////////////////////////////////
 // Public Methods
@@ -18,28 +26,28 @@ exports.presentObject = (object) => ({ status: 200, data: object });
 
 exports.presentCreatedObject = (object) => ({ status: 201, data: object });
 
-exports.presentConflict = (errorObj) => {
-  const message = (Array.isArray(errorObj) && errorObj.length > 0) ? errorObj[0] : errorObj;
-  return { status: 409, data: { code: 409, message } };
-};
+exports.presentNotAuthorized = () => ({ status: 401, data: DATA_NOT_AUTHORIZED });
 
-exports.presentNotAuthorized = () => (MSG_NOT_AUTHORIZED);
+exports.presentNotAuthenticated = () => ({ status: 403, data: DATA_NOT_AUTHENTICATED });
 
-exports.presentNotAuthenticated = () => (MSG_NOT_AUTHENTICATED);
-
-exports.presentObjectNotFound = () => (MGS_OBJECT_NOT_FOUND);
+exports.presentObjectNotFound = () => ({ status: 404, data: DATA_OBJECT_NOT_FOUND });
 
 exports.presentObjectIfFound = (object) => {
-  const result = (object) ? { code: 200, data: object } : MGS_OBJECT_NOT_FOUND;
+  const result = (object) ? exports.presentObject(object) : exports.presentObjectNotFound();
   return result;
 };
 
 exports.presentResultOfDeletion = (wasDeleted) => {
-  const result = (wasDeleted) ? MSG_OBJECT_DELETED : MGS_OBJECT_NOT_FOUND;
+  const result = (wasDeleted) ? ({ status: 204, data: null }) : exports.presentObjectNotFound();
   return result;
 };
 
 exports.presentResultOfRelationshipDeletion = (wasDeleted) => {
-  const result = (wasDeleted) ? MSG_OBJECT_DELETED : MSG_RELATIONSHIP_NOT_FOUND;
+  const result = (wasDeleted) ? ({ status: 204, data: null }) : ({ status: 404, data: DATA_RELATIONSHIP_NOT_FOUND });
   return result;
+};
+
+exports.presentConflict = (errorObj) => {
+  const message = getFirstErrorMessage(errorObj);
+  return { status: 409, data: buildDataWithMessage(409, message) };
 };
