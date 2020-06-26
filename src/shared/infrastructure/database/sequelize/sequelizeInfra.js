7e31830d658791235e38afc4fc0db58c7cc72d0b
Diego Perez <diegopm2000@gmail.com>
2020-06-26 13:52:50 +0200

    wip

--- src/shared/infrastructure/database/sequelize/sequelizeInfra.js
+++ src/shared/infrastructure/database/sequelize/sequelizeInfra.js
@@ -1,20 +1,53 @@
 // sequelizeInfra.js
 
+/* eslint-disable import/no-dynamic-require */
+/* eslint-disable global-require */
+
+const glob = require('glob');
+
 const { Sequelize } = require('sequelize');
 
-const SequelizeGamesystemModel = require('../../../../gamesystem/infrastructure/repository/sequelize/SequelizeGamesystemModel');
-const SequelizeVideogameModel = require('../../../../videogame/infrastructure/repository/sequelize/SequelizeVideogameModel');
+const logger = require('../../log/logFacade');
 
 // //////////////////////////////////////////////////////////////////////////////
 // Properties & Constants
 // //////////////////////////////////////////////////////////////////////////////
 
+const MODULE_NAME = '[sequelizeInfra]';
+
 let sequelize;
-const models = {};
+
+// //////////////////////////////////////////////////////////////////////////////
+// Private Functions
+// //////////////////////////////////////////////////////////////////////////////
+
+// TODO estos dos metodos se pueden sacar a una libreria de utilidad
+
+const getSequelizeModels = () => {
+  logger.debug(`${MODULE_NAME} getSequelizeModels (IN) --> no params`);
+
+  const result = glob.sync('src/**/Sequelize*Model.js');
+
+  logger.debug(`${MODULE_NAME} getSequelizeModels (OUT) --> result: ${JSON.stringify(result)}`);
+  return result;
+};
+
+const loadModule = (pathfile) => {
+  logger.debug(`${MODULE_NAME} loadModule (IN) --> pathfile: ${pathfile}`);
+
+  const realPath = `../../../../../${pathfile}`;
+  const module = require(realPath);
+  logger.debug(`${MODULE_NAME} loadModule (MID) --> module loaded`);
+
+  logger.debug(`${MODULE_NAME} loadModule (OUT) --> module: <<module>>`);
+  return module;
+};
 
 // //////////////////////////////////////////////////////////////////////////////
 // Public Functions
 // //////////////////////////////////////////////////////////////////////////////
+
+// TODO cargar los modelos de forma dinámica
 
 exports.init = (options) => {
   sequelize = new Sequelize(options.dbname, options.dbuser, options.dbpasswd, {
@@ -25,13 +58,17 @@ exports.init = (options) => {
     },
   });
 
-  models.gameSystemModel = SequelizeGamesystemModel.init(sequelize);
-  models.videogameModel = SequelizeVideogameModel.init(sequelize);
+  const models = getSequelizeModels();
 
-  models.gameSystemModel.initAssociations();
-  models.videogameModel.initAssociations();
+  models.forEach((x) => {
+    const module = loadModule(x);
+    module.init(sequelize);
+  });
+
+  models.forEach((x) => {
+    const module = loadModule(x);
+    module.initAssociations(sequelize);
+  });
 };
 
 exports.getSequelize = () => sequelize;
-
-exports.getModel = (nameModel) => models[nameModel];
